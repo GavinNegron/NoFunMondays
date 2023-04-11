3a5a9ee3ceb1b915a29362227944e22d6e1c68d7
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2023-04-10 23:50:46 -0400

    Started Dashboard

--- public/js/app/modules/loader.js
+++ public/js/app/modules/loader.js
@@ -3,12 +3,16 @@ import navbar from '/js/app/modules/navbar.js';
 
 const loader = (() => {
     const init = () => {
-        SetMainHeight();
-        $(window).on('resize', () => {
-            SetMainHeight();
-        });
-
+        const footerElement = document.querySelector('footer');
         const navbarElement = document.querySelector('.navbar');
+        if (navbarElement && footerElement) {
+            // Set min-height of MAIN
+            SetMainHeight();
+            $(window).on('resize', function() {
+                SetMainHeight();
+            });
+        }
+
         if (navbarElement) {
             navbar.init();
         }
