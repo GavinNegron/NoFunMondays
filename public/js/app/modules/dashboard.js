a7e323a5e9bc2c509279e212c7e4794813588e42
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2023-04-20 21:20:09 -0400

    Fixed issues with sidebar scroll

--- public/js/app/modules/dashboard.js
+++ public/js/app/modules/dashboard.js
@@ -1,6 +1,9 @@
 const dashboard = (() => {
     const init = () => {
-        
+      if (window.innerWidth < 992) {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.classList.add('active');
+      }
     };
   
     return {
