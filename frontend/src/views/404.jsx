7db367a7e7339dc169cb620484dd48e3f23b3ed1
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2024-11-21 17:46:56 -0500

    .

--- frontend/src/views/404.jsx
+++ frontend/src/views/404.jsx
@@ -6,18 +6,18 @@ function NotFound() {
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
     <link rel="stylesheet" href="/css/404.css"></link>
 
-      <div className="error-404">
-        <div className="error-404__header">
+      <div class="error-404">
+        <div class="error-404__header">
             <span>404</span>
         </div>
-        <div className="error-404__description">
+        <div class="error-404__description">
             <span>Page Not Found</span>
         </div>
-        <div className="error-404__pun">
+        <div class="error-404__pun">
             <span>Looks like this page has gone AFK</span>
         </div>
-        <div className="error-404__btn">
-          <div className="fortnite__btn">
+        <div class="error-404__btn">
+          <div class="fortnite__btn">
             <span><a href="/">Return To Lobby</a></span>
           </div>
         </div>
--- frontend/src/views/templates/footer.jsx
+++ frontend/src/views/templates/footer.jsx
@@ -5,18 +5,18 @@ function Footer() {
 
   return (
     <>
-    <footer className="footer">
-    <div className="container-fluid">
-        <div className="footer__top center">
-            <div className="footer__logo">
+    <footer class="footer">
+    <div class="container-fluid">
+        <div class="footer__top center">
+            <div class="footer__logo">
                 <p>NoFunMondays</p>
             </div>
-            <div className="footer__socials">
+            <div class="footer__socials">
                 <Socials />
             </div>
         </div>
-        <div className="footer__bottom center">
-            <div className="footer__copyright">
+        <div class="footer__bottom center">
+            <div class="footer__copyright">
                 <span>&copy; 2024 Lorem, ipsum. All rights reserved.</span>
             </div>
         </div>
