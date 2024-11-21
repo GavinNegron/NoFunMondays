2371e5f46e7dd5d41ecb6d56f84fe4e0d0bb57de
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2024-11-21 17:49:43 -0500

    .

--- backend/server.js
+++ backend/server.js
@@ -29,14 +29,8 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 // Enable CORS for the frontend (React) on port 3000 during development
-=======
->>>>>>> parent of fb91f89 (Updated featured post to not show up in recent posts)
-=======
->>>>>>> parent of fb91f89 (Updated featured post to not show up in recent posts)
 app.use(cors({
     origin: 'http://localhost:3000', // Allow the frontend on port 3000 during dev
     methods: ['GET', 'POST'],
