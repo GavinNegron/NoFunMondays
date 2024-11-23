c9fc7a2ddbffb8285c13526c356c717a7eae6f04
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2024-11-22 21:18:59 -0500

    Update env

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Make sure dotenv is loaded
 module.exports = {
     googleCloud: {
         keyFilename: process.env.GCLOUD_FILE,  // Directly use the value from .env
-        projectId: process.env.GCLOUD_PROJECTID,  // Directly use the value from .env
-        bucketName: process.env.GCLOUD_BUCKETNAME,  // Directly use the value from .env
+        projectId: 'nofunmondays',  // Directly use the value from .env
+        bucketName: 'nofunmondays',  // Directly use the value from .env
     },
 };
