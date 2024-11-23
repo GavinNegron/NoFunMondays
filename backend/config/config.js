3de6f14ae06e778e1153787a6086865a341ac8d1
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2024-11-22 21:03:26 -0500

    Updated env.

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+require('dotenv').config(); // Make sure dotenv is loaded
 
 module.exports = {
     googleCloud: {
-        keyFilename: path.join(__dirname, 'nofunmondays-c00d914343f2.json'),
-        projectId: 'nofunmondays', 
-        bucketName: 'nofunmondays', 
+        keyFilename: process.env.GCLOUD_FILE,  // Directly use the value from .env
+        projectId: process.env.GCLOUD_PROJECTID,  // Directly use the value from .env
+        bucketName: process.env.GCLOUD_BUCKETNAME,  // Directly use the value from .env
     },
 };
