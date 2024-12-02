be3ca41f222a6ba7fd761611251676dcb459d01b
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2024-12-02 12:02:47 -0500

    update

--- frontend/src/views/dashboard/edit/edit-post.jsx
+++ frontend/src/views/dashboard/edit/edit-post.jsx
@@ -1,100 +1,141 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import NotFound from '../../404/404'
-import Navbar from '../../layout/navbar'
-import { Helmet } from 'react-helmet-async'
-import loading from '../../../utilities/loading'
-import LoadingScreen from '../../templates/base/loading'
-import EditorSidebar from './edit-sidebar'
-import EditorNavbar from './edit-navbar'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import NotFound from '../../404/404';
+import Navbar from '../../layout/navbar';
+import { Helmet } from 'react-helmet-async';
+import loading from '../../../utilities/loading';
+import LoadingScreen from '../../templates/base/loading';
+import EditorSidebar from './edit-sidebar';
+import EditorNavbar from './edit-navbar';
+import EditorNavbar2 from './edit-editornav';
+import $ from 'jquery';
 
-function BlogPost() { 
-  const { slug } = useParams()
-  const [post, setPost] = useState(null)
-  const [loadingState, setLoadingState] = useState(true)
-  const [notFound, setNotFound] = useState(false)
+function BlogPost() {
+  const { slug } = useParams();
+  const [post, setPost] = useState(null);
+  const [loadingState, setLoadingState] = useState(true);
+  const [notFound, setNotFound] = useState(false);
+  const [elements, setElements] = useState([]);
 
   useEffect(() => {
     const handleLoading = async () => {
-      await Promise.all([loading(['/css/blog-post.css']), new Promise(resolve => setTimeout(resolve, 500))])
+      await Promise.all([loading(['/css/edit-post.css']), new Promise(resolve => setTimeout(resolve, 500))]);
 
       try {
-        const response = await fetch('/api/posts')
+        const response = await fetch('/api/posts');
         if (!response.ok) {
-          throw new Error('Failed to fetch posts')
+          throw new Error('Failed to fetch posts');
         }
 
-        const posts = await response.json()
-        const matchedPost = posts.find(p => p.slug === slug)
+        const posts = await response.json();
+        const matchedPost = posts.find(p => p.slug === slug);
 
         if (matchedPost) {
-          setPost(matchedPost)
+          setPost(matchedPost);
+          setElements([
+            { id: 'img', type: 'image', draggable: false, content: matchedPost.imageUrl },
+            { id: 'text1', type: 'h1', content: matchedPost.title }
+          ]);
         } else {
-          setNotFound(true)
+          setNotFound(true);
         }
       } catch (error) {
-        setNotFound(true)
+        setNotFound(true);
       } finally {
-        setLoadingState(false)  
+        setLoadingState(false);
       }
+    };
+
+    handleLoading();
+  }, [slug]);
+
+  const handleDragStart = (e, type) => {
+    e.dataTransfer.setData("text/plain", type);
+  };
+
+  const handleDrop = (e, targetId) => {
+    e.preventDefault();
+    const draggedType = e.dataTransfer.getData("text/plain");
+    const newElement = {
+      id: `${draggedType}-${Date.now()}`,
+      type: draggedType,
+      content: `New ${draggedType.toUpperCase()}`
+    };
+
+    const targetIndex = elements.findIndex((el) => el.id === targetId);
+    const updatedElements = [...elements];
+    updatedElements.splice(targetIndex, 0, newElement);
+    setElements(updatedElements);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const renderElement = (element) => {
+    switch (element.type) {
+      case 'image':
+        return <img src={element.content} alt="Dynamic Element" />;
+      case 'header':
+        return <h1>{element.content}</h1>;
+      case 'text':
+        return <p>{element.content}</p>;
+      default:
+        return <div>{element.content}</div>;
     }
+  };
 
-    handleLoading()
-  }, [slug])
+  const handleBlogPostElement = () => {
+    $('.editornav').stop(true, true).fadeIn()
 
-  if (loadingState) {
-    return <LoadingScreen />
+    $('.blog-post-content').addClass('navActive')
   }
-
-  if (notFound) {
-    return <NotFound />
-  }
-
+  const handleClickOutside = (event) => {
+    if (!event.target.closest('.addElement')) {
+      $('.editor-sidebar__add-elements').stop(true, true).animate({}).fadeOut(200);
+    }
+  };
+  
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+  
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
   return (
-    <>   
-      <Helmet>
-        <title>{post.title}</title>
-        <link rel="stylesheet" href="/css/edit-post.css" />
-      </Helmet>
-      <Navbar />
-      <main className="main">
-        <EditorNavbar key={post._id} post={post}/>
-        <EditorSidebar/>
-        <div className="editor-container">
-          <div className="editor">
-              <div className="editor__back">
-                  <a href="/dashboard/posts">
-                    <i class="fa-solid fa-arrow-left"></i>
-                    <span>Dashboard</span>
-                  </a>
-              </div>
-           </div>
-          <div className="post">
-            <div className="post__inner">
-              <div className="post__inner__content">
-                <div className="item item-img">
-                  <img src={post.imageUrl} alt={post.title} />
+    <div className="blog-post-container">
+      {loadingState && <LoadingScreen />}
+      {notFound ? (
+        <NotFound />
+      ) : (
+        <>
+          <Helmet>
+            <title>{post?.title || 'Blog Post'}</title>
+          </Helmet>
+          <Navbar />
+          <EditorNavbar post={post} />
+          <EditorNavbar2/>
+          <EditorSidebar handleDragStart={handleDragStart} />
+          <div className="blog-post-content">
+            <div className="blog-post-main">
+              {elements.map((element) => (
+                <div
+                  key={element.id}
+                  className={`blog-post-element ${element.type}`}
+                  onDrop={(e) => handleDrop(e, element.id)}
+                  onDragOver={handleDragOver}
+                  onClick={handleBlogPostElement}
+                >
+                  {renderElement(element)}
                 </div>
-                <div className="post__inner__content__header">
-                  <div className="item item-header">
-                    <header>{post.title}</header>
-                  </div>
-                </div>
-                <div className="post__inner__content__description">
-                  <div className="item item-text">
-                    <p>{post.description}</p>
-                  </div>
-                  <div className="item item-text">
-                    <p>test</p>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
-        </div>
-      </main>
-    </>
-  )
+        </>
+      )}
+    </div>
+  );
 }
 
-export default BlogPost
+export default BlogPost;
--- frontend/src/views/dashboard/edit/edit-sidebar.jsx
+++ frontend/src/views/dashboard/edit/edit-sidebar.jsx
@@ -2,23 +2,22 @@ import React, { useState } from 'react';
 import Tooltip from '../../../utilities/tooltip';
 import $ from 'jquery';
 
-function EditSidebar() {
-  const [isAddElementsVisible, setIsAddElementsVisible] = useState(false);
+function EditSidebar({ handleDragStart }) {
+  const [activeElement, setActiveElement] = useState('text'); // Set "text" as the default active element
 
   const handleAddElementsClick = () => {
-    const element = $('.editor-sidebar__add-elements');
-    
-      element.stop(true, true).animate({
-      }).fadeToggle(200);
-    
-    setIsAddElementsVisible(!isAddElementsVisible);  
+    $('.editor-sidebar__add-elements').stop(true, true).animate({}).fadeToggle(200);
+  };
+
+  const handleElementClick = (elementType) => {
+    setActiveElement(elementType);
   };
 
   return (
     <>
       <aside className="editor-sidebar">
         <div className="editor-sidebar__icons">
-          <div className="editor-sidebar__icons__item">
+          <div className="editor-sidebar__icons__item addElement">
             <button onClick={handleAddElementsClick}>
               <i data-tooltip-id="tip-blocks" className="fa-solid fa-plus"></i>
               <Tooltip id="tip-blocks" header="Add elements" place="right" />
@@ -30,22 +29,70 @@ function EditSidebar() {
               <div className="d-flex flex-direction-row col-12">
                 <div className="editor-sidebar__add-elements__left col-4">
                   <ul>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Image</a></li>
-                    <li><a href="">Button</a></li>
-                    <li><a href="">Header</a></li>
-                    <li><a href="">Icon</a></li>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Text</a></li>
-                    <li><a href="">Text</a></li>
+                    <li><a onClick={() => handleElementClick('text')}>Text</a></li>
+                    <li><a onClick={() => handleElementClick('image')}>Image</a></li>
+                    <li><a onClick={() => handleElementClick('button')}>Button</a></li>
+                    <li><a onClick={() => handleElementClick('header')}>Header</a></li>
+                    <li><a onClick={() => handleElementClick('icon')}>Icon</a></li>
+                    <li><a onClick={() => handleElementClick('form')}>Form</a></li>
+                    <li><a onClick={() => handleElementClick('video')}>Video</a></li>
+                    <li><a onClick={() => handleElementClick('blank')}>Blank Div</a></li>
                   </ul>
                 </div>
                 <div className="editor-sidebar__add-elements__right col-8">
-                  right
+                  {activeElement === 'text' && (
+                    <div className="editor-sidebar__add-elements__right__text">
+                      <a
+                        className="text"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'text')}
+                      >
+                        Add default text 
+                      </a>
+                      <a
+                        className="h1"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'h1')}
+                      >
+                        Add Heading 1
+                      </a>
+                      <a
+                        className="h2"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'h2')}
+                      >
+                        Add Heading 2
+                      </a>
+                      <a
+                        className="h3"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'h3')}
+                      >
+                        Add Heading 3
+                      </a>
+                      <a
+                        className="h4"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'h4')}
+                      >
+                        Add Heading 4
+                      </a>
+                      <a
+                        className="h5"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'h5')}
+                      >
+                        Add Heading 5
+                      </a>
+                      <a
+                        className="h6"
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, 'h6')}
+                      >
+                        Add Heading 6
+                      </a>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
