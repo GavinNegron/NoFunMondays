b86427074f488a011d2ee93cedac8e961527ecb1
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-01-16 19:30:45 -0500

    1.0.0

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://nofunmondays.com'],
         methods: ['GET', 'POST'], 
         allowedHeaders: ['Content-Type'], 
     })
