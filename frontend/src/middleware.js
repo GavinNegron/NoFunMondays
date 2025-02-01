2af4db5d70e3586d1d1884696ea0859b55546810
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-02-01 11:59:03 -0500

    Updated middleware

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,23 +1,35 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { jwtDecode } from 'jwt-decode';
 
 export async function middleware(request) {
     try {
-        const isAuthenticated = request.cookies.get('is_auth')?.value;
+        const token = request.cookies.get('accessToken')?.value;
 
-        if (isAuthenticated && request.nextUrl.pathname === '/login') {
-            return NextResponse.redirect(new URL('/dashboard', request.url));
+        if (!token) {
+            if (request.nextUrl.pathname === '/login') {
+                return NextResponse.next(); 
+            }
+            return NextResponse.redirect(new URL('/login', request.url)); 
         }
 
-        if (!isAuthenticated && request.nextUrl.pathname !== '/login') {
-            return NextResponse.redirect(new URL('/login', request.url));
+        const decoded = jwtDecode(token); 
+        const isTokenExpired = decoded.exp * 1000 < Date.now(); 
+
+        if (isTokenExpired) {
+            return NextResponse.redirect(new URL('/login', request.url)); 
         }
 
-        return NextResponse.next();
+        if (request.nextUrl.pathname === '/login') {
+            return NextResponse.redirect(new URL('/dashboard', request.url)); 
+        }
+
+        return NextResponse.next();  
     } catch (error) {
-        console.error('Error occurred while checking authentication: ', error);
+        console.error('Error in middleware:', error);
+        return NextResponse.redirect(new URL('/login', request.url)); 
     }
 }
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/login']
-}
+    matcher: ['/dashboard/:path*', '/admin/:path*', '/login']  
+};
