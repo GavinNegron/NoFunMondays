56eebc10b2f8969034ff6696310219119d0ba6f8
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-02-11 15:31:21 -0500

    Update socials links

--- frontend/src/components/base/socials.jsx
+++ frontend/src/components/base/socials.jsx
@@ -4,10 +4,10 @@ function Socials() {
 return (
     <>   
     <div className="socials">
-        <Link className="socials-item" href='/discord' data-color="#7289da"><i className="fa-brands fa-discord"></i></Link>
-        <Link className="socials-item" href='/youtube' data-color="#ff0000"><i className="fa-brands fa-youtube"></i></Link>
-        <Link className="socials-item" href='/twitter' data-color="#1da1f2"><i className="fa-brands fa-twitter"></i></Link>
-        <Link className="socials-item" href='/email' data-color="#333333"><i className="fa-regular fa-envelope"></i></Link>
+        <Link target="_blank" className="socials-item" href='/discord' data-color="#7289da"><i className="fa-brands fa-discord"></i></Link>
+        <Link target="_blank" className="socials-item" href='/youtube' data-color="#ff0000"><i className="fa-brands fa-youtube"></i></Link>
+        <Link target="_blank" className="socials-item" href='https://x.com/NoFunMondays' data-color="#1da1f2"><i className="fa-brands fa-twitter"></i></Link>
+        <Link target="_blank" className="socials-item" href='/contact' data-color="#333333"><i className="fa-regular fa-envelope"></i></Link>
     </div>
       </>
     )
