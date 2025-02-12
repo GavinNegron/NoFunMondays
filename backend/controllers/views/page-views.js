289e4324d73acde93f1f22e5f8bb4332b1ccfa85
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-02-12 17:13:51 -0500

    Test

--- backend/controllers/views/page-views.js
+++ backend/controllers/views/page-views.js
@@ -9,7 +9,7 @@ const hashUser = (ip, userAgent) => {
 const pageViews = async (req, res) => {
   const { slug } = req.query;
   const ip = (req.headers['x-forwarded-for'] || req.ip).split(',')[0].trim();
-  console.log("USER IP \n is IP: \n", ip, "\n")
+  console.log("USER IP \n is IP: \n", ip, "\n");
   const userAgent = req.headers['user-agent'] || 'unknown';
   const userHash = hashUser(ip, userAgent);
 
@@ -18,16 +18,14 @@ const pageViews = async (req, res) => {
   }
 
   try {
-    const existingView = await PageView.findOne({ postSlug: slug, userHash });
+    const existingView = await PageView.findOne({ postSlug: slug, userId: userHash });
+
     console.log("Stored Hashes for this Post:", await PageView.find({ postSlug: slug }));
     console.log("Current User Hash:", userHash);
+
     if (!existingView) {
       await PageView.create({ postSlug: slug, userId: userHash });
-      await PageView.updateOne(
-        { postSlug: slug, userId: userHash }, 
-        { $inc: { count: 1 } }, 
-        { upsert: true }
-      );      await Posts.updateOne({ slug }, { $inc: { views: 1 } });
+      await Posts.updateOne({ slug }, { $inc: { views: 1 } });
     }
 
     const totalViews = await PageView.countDocuments({ postSlug: slug });
