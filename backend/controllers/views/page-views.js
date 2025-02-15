b92f47c100f0b2c4094d1d046810a870a17e5569
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-02-15 11:28:58 -0500

    Update

--- backend/controllers/views/page-views.js
+++ backend/controllers/views/page-views.js
@@ -2,12 +2,16 @@ const UAParser = require('ua-parser-js');
 const Posts = require('../../models/Posts');
 const PageView = require('../../models/PageView');
 const geoip = require('geoip-lite');
+const { v4: uuidv4 } = require('uuid');
 
 const pageViews = async (req, res) => {
   const { slug } = req.query;
   const ipAddress = (req.headers['x-forwarded-for'] || req.ip).split(',')[0].trim();
   const userAgent = req.headers['user-agent'] || 'unknown';
   const referrer = req.headers['referer'] || 'direct';
+  const userId = req.cookies.userId || uuidv4();
+
+  res.cookie('userId', userId, { maxAge: 31536000000, httpOnly: true }); // Set cookie for 1 year
 
   const parser = new UAParser(userAgent);
   const browser = parser.getBrowser().name || 'unknown';
@@ -22,10 +26,10 @@ const pageViews = async (req, res) => {
   }
 
   try {
-    const existingView = await PageView.findOne({ postSlug: slug, ipAddress });
+    const existingView = await PageView.findOne({ postSlug: slug, userId });
 
     if (!existingView) {
-      await PageView.create({ postSlug: slug, ipAddress, browser, referrer, region, timezone });
+      await PageView.create({ postSlug: slug, ipAddress, browser, referrer, region, timezone, userId });
       await Posts.updateOne({ slug }, { $inc: { views: 1 } });
     }
 
