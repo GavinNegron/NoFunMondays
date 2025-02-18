0bf5e4232b7a87ddc23d96720b9e4b67d3a1a3be
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-02-18 16:25:48 -0500

    Update bsky

--- frontend/src/components/base/socials/index.jsx
+++ frontend/src/components/base/socials/index.jsx
@@ -6,7 +6,7 @@ return (
     <>   
     <div className="socials">
         <Link target="_blank" className="socials-item" href='https://www.youtube.com/@NoFunMondaysFN' id="youtube"><i className="fa-brands fa-youtube"></i></Link>
-        <Link target="_blank" className="socials-item" href='https://bsky.app/profile/nofunmondaysfn.bsky.social' id="bluesky"><i className="fa-brands fa-bluesky"></i></Link>
+        <Link target="_blank" className="socials-item" href='https://bsky.app/profile/nofunmondays.com' id="bluesky"><i className="fa-brands fa-bluesky"></i></Link>
         <Link target="_blank" className="socials-item" href='https://www.pinterest.com/nofunmondaysblog/_created' id="pinterest"><i className="fa-brands fa-pinterest"></i></Link>
         <Link target="_blank" className="socials-item" href='/contact' id="email"><i className="fa-regular fa-envelope"></i></Link>
     </div>
