c2e54f5bd87e35d6f510f0acaa7ed47111e4bf5a
GavinNegron <70559179+GavinNegron@users.noreply.github.com>
2025-02-18 14:58:52 -0500

    Update favicon

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -15,6 +15,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <meta property="og:type" content="website"/>
         <meta name="twitter:title" content="NoFunMondays"/>
+        <link rel="icon" href="https://nofunmondays.com/images/NoFunMondays.png" />
       </Head>
       <Provider store={store}>
         {isDashboardPostsPage ? (
